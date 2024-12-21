7723532e02bce4d5a00c9d09c7db9cce25a86cd9
yuhi-dev <yuhiro.asukabe@gmail.com>
2024-12-21 22:51:31 +0900

    commit

--- ABC/385/C.cpp
+++ ABC/385/C.cpp
@@ -14,11 +14,14 @@ int main() {
     int maxBuildings = 1;
     for (int d = 1; d < N; ++d) {
         unordered_map<int, int> count;
-        for (int i = 0; i + d < N; ++i) {
+        for (int i =0;i + d < N; ++i) {
             if (H[i] == H[i + d]) {
                 count[H[i]]++;
                 maxBuildings = max(maxBuildings, count[H[i]]);
-                cout << count[H[i]] << endl;
+            }
+            else if(H[i] == H[i + d] + 1) {
+                count[H[i]]++;
+                maxBuildings = max(maxBuildings, count[H[i]]);
             }
         }
     }
@@ -26,3 +29,29 @@ int main() {
     cout << maxBuildings << endl;
     return 0;
 }
+
+//answer
+/*#include <bits/stdc++.h>
+using namespace std;
+int n,a[3010],ans=0,mx=0;
+int main(){
+	cin>>n;
+	if(n==1) {
+		cout<<1<<endl;
+		return 0;
+	}
+	for(int i=1; i<=n; ++i) cin>>a[i];
+	for(int i=1; i<=n; ++i){
+		for(int j=1; j<=n-i; ++j){
+			int k=i;
+			ans=1;
+			while(k+j<=n){
+				if(a[k+j]!=a[k]) break;
+				k+=j,ans++; 
+			}
+			mx=max(ans,mx);
+		}
+	}
+	cout<<mx<<endl;
+	return 0;
+}*/
